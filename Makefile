b1f27506e55a3ade7494728aa69ba85c9668547b
Fionace <xuce1260505@126.com>
2012-12-02 19:51:59 +0800

    init version

---
+++ Makefile
@@ -0,0 +1,3 @@
+all:t1
+t1:t1.cpp
+	g++ -g -o t1 t1.cpp;
---
+++ t1.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<bitset>
+using namespace std;
+int main()
+{
+    /* tell me how to
+     do it  
+    cout<<*"<<" ";
+    cout<<"*"<<" ";
+//    cout<< "" <<" ";
+      cout<<"xuce "<<endl;*/ 
+  //  for(int i=0;i!=10;i++)
+/*  int value,sum=0;
+  while(cin>>value)
+    {
+        sum+=value;
+        //cout<<value<<" ";
+    }*/
+/*    vector<string> s(1,"hello");
+    vector<string> a(1,"xuce");
+    if(s>a)
+        cout<<"s is bigger than a!"<<endl;
+    else if(s==a)
+        cout<<"s is equel to a!"<<endl;
+    else
+      cout<<"s is smaller than a!"<<endl;
+*/
+//    bitset<32> vec(0x1011);
+ // cout<<vec<<endl;
+// vector<int>::iterator iter=(s.begin()+s.end())/2;
+ //cout<<*iter<<endl;
+    /*if(s.empty())
+    cout<<"Is empty!"<<endl;
+    else
+        cout<<s[0]<<"aa"<<endl;
+   */ 
+    char *a=new char[5];
+    cin>>a;
+    cout<<a<<endl;
+}
